5297dc426212f4515c2c5681889730d36f97aafe
Lelozitos <lm.fabre@hotmail.com>
2024-12-02 13:54:19 -0300

    Initial Tests

---
+++ src/demo.py
@@ -0,0 +1,14 @@
+from elevator_controller import ElevatorController
+import time
+
+# verbose = True
+# verboseprint = print if verbose else lambda *a, **k: None
+
+controller = ElevatorController(1, 0, 5)
+
+controller.run()
+controller.add_requests([5, 1])
+time.sleep(1)
+controller.add_requests([3])
+time.sleep(40)
+controller.add_requests([2])
